f629651d48bbcf6a95731d170744af024e8336d1
Mohameddshawky <147840781+Mohameddshawky@users.noreply.github.com>
2025-09-20 20:54:32 +0300

    create create

--- Company.PL/Controllers/DepartmentController.cs
+++ Company.PL/Controllers/DepartmentController.cs
@@ -1,5 +1,7 @@
 ﻿using Company.BLL.Interfaces;
 using Company.BLL.Repositories;
+using Company.DAL.Models;
+using Company.PL.DTos;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Company.PL.Controllers
@@ -18,6 +20,28 @@ namespace Company.PL.Controllers
            
             var departments=departmentRepository.GetAll().ToList();
             return View(departments);
+        }
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Create(CreateDepartmentDto model)
+        {
+            if (ModelState.IsValid) {//server side validation
+                var Department = new Departments
+                {
+                    Name = model.Name,
+                    Code = model.Code,
+                    CreateAt = model.CreateAt,
+                };
+               var cnt= departmentRepository.Add(Department);
+                if (cnt > 0) {
+                    return RedirectToAction(nameof(Index));
+                }
+            }
+            return View();
         }
     }
 }
