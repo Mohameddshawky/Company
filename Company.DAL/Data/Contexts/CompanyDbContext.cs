55ec02039769e55496ddd575cc9cda09909085f5
Mohameddshawky <147840781+Mohameddshawky@users.noreply.github.com>
2025-09-20 16:35:18 +0300

    imp of Dal

---
+++ Company.DAL/Data/Contexts/CompanyDbContext.cs
@@ -0,0 +1,28 @@
+﻿using Company.DAL.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Company.DAL.Data.Contexts
+{
+    internal class CompanyDbContext:DbContext
+    {
+        public CompanyDbContext():base()
+        {
+            
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+        }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server = SHAWKY\\MSQLSERVER ;Database= project1 ; Trusted_Connection =True ; TrustServerCertificate=true");
+        }
+        public DbSet<Departments> Departments { get; set; }
+    }
+}
