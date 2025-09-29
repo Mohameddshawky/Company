5ac0131886500e1c3d78994e3b3f91fd3a468c4d
mohamed shawky <shawky1mohamed2@gmail.com>
2025-09-29 19:03:23 +0300

    notcomplete

---
+++ Company.BLL/AttachmentService/AttachmentService.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Company.BLL.AttachmentService
+{
+    internal class AttachmentService : IAttachmentService
+    {
+        public bool Delete(string folderPath)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string? Upload(IFormFile file, string folderName)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
